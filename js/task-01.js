f7f1b1a2b3832a67e3d2e90cf35642cde26c8d47
Mohammed BarBar <mohammedking71@gmail.com>
2022-10-02 15:13:45 +0300

    Change on Console

--- js/task-01.js
+++ js/task-01.js
@@ -2,9 +2,9 @@ const categroy = document.querySelector(".categories");
 let item = document.querySelectorAll(".item");
 const innerItems = document.querySelectorAll(".item > ul");
 const topic = document.querySelectorAll(".item > h2");
-console.log("Number of categories :" + item.length);
+console.log("Number of categories:", item.length);
 
 for (let index = 0; index < topic.length; index++) {
-  console.log("Category : " + topic[index].innerHTML);
-  console.log("Elements : " + innerItems[index].querySelectorAll("li").length);
+  console.log("Category:", topic[index].innerHTML);
+  console.log("Elements:", innerItems[index].querySelectorAll("li").length);
 }
